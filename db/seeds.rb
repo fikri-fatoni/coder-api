0f599c04a40b44e4987bc77a90c182f09d3449ad
fikri.fatoni <fikri.fatoni@doterb.com>
2020-10-15 17:56:57 +0700

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+admin = User.new
+admin.email = 'admin@coind.co.id'
+admin.username = 'admin'
+admin.password = 'password'
+admin.password_confirmation = 'password'
+admin.first_name = 'Admin'
+admin.last_name = 'Coder'
+admin.date_of_birth = '17/08/1945'
+admin.phone_number = '081800008888'
+admin.programming_skill = 5
+admin.save
+
+user.add_role(:admin)
